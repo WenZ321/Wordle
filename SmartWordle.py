8427c908fbd37b43b2d172d4da38590ca8cde564
Wen Zhang <wzhang50@stuy.edu>
2024-02-06 00:32:59 -0500

    Started basic outlining

---
+++ SmartWordle.py
@@ -0,0 +1,14 @@
+
+# Libraries
+
+# Generating the website
+def generateWebsite():
+    return 0;
+
+# Guessing mechanic
+def guessing():
+    return 0;
+
+# Generating a random word
+def GenerateWord():
+    return ""; 
