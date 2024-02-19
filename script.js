441cac1f85416f5d79d38ddf923f423df2a6069b
Wen Zhang <wzhang50@stuy.edu>
2024-02-18 23:34:15 -0500

    Added guessing

--- script.js
+++ script.js
@@ -37,18 +37,32 @@ document.addEventListener('DOMContentLoaded', () => {
         } else if (key === '←') {
             input.value = input.value.slice(0, -1);
         } else if (input.value.length < 5) {
-            input.value += key;
+            input.value += key.toUpperCase();
         }
     }
+
+
+    let currentAttempt = 0;
 
     window.submitGuess = () => {
         const guess = input.value.toUpperCase();
         if (guess.length === 5) {
             console.log("Guess submitted: ", guess);
+            displayGuessOnGrid(guess)
             input.value = ''; 
             message.textContent = ''; 
+            currentAttempt++;
         } else {
             message.textContent = "Please enter a 5-letter word.";
         }
     };
+
+    function displayGuessOnGrid(guess) {
+        const startIdx = currentAttempt * 5;
+        const boxes = document.querySelectorAll('.wordle-box');
+        for (let i = 0; i < guess.length; i++) {
+            boxes[startIdx + i].textContent = guess[i];
+        }
+    }
+
 });
